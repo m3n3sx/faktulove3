9058f5acef721fb479d2b0732b52847343162554
FaktuLove Admin <admin@faktulove.pl>
2025-08-30 05:06:12 +0200

    Fix deployment configuration

---
+++ settings_deploy.py
@@ -0,0 +1,20 @@
+import os
+from faktulove.settings import *
+
+DEBUG = False
+ALLOWED_HOSTS = ['*']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+] + MIDDLEWARE
